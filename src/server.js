9bc56a6df24c700a5a84960ac02bd800e6409a45
mateus-kent <mateusquent2003@gmail.com>
2022-12-05 18:37:01 -0300

    feat: organize folders

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cookieParser = require('cookie-parser')
+
+const routes = require("./routes/routes");
+
+const server = express();
+
+server.set("view engine", "ejs");
+
+server.set("views", __dirname + "/views/");
+
+server.use(express.static("public"));
+server.use(express.json());
+
+server.use(express.urlencoded({ extended: true }));
+
+server.use(
+  cookieParser()
+);
+
+server.use(routes);
+
+server.listen(3001, () =>
+  console.log("✨ Desaparecidos (WEB) > Running on localhost:3001 ✨")
+);
